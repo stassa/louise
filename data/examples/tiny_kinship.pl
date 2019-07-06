173d550e83e9113a694aead0581b1c78162db335
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-07-06 14:27:02 +0100

    Added some new relations to tiny_kinship.pl * And that makes it not so tiny any more.

--- data/examples/tiny_kinship.pl
+++ data/examples/tiny_kinship.pl
@@ -9,6 +9,9 @@
 		       ,parent/2
 		       ,husband/2
 		       ,wife/2
+		       ,child/2
+		       ,son/2
+		       ,daughter/2
 		       ,father/2
 		       ,mother/2
 		       ,male/1
@@ -25,11 +28,15 @@ Includes examples for ancestor/2, father/2, grandfather/2 and male/2.
 background_knowledge(ancestor/2,[father/2,mother/2,parent/2]).
 background_knowledge(grandparent/2,[father/2,mother/2,parent/2]).
 background_knowledge(grandfather/2,[father/2,mother/2,parent/2]).
-%background_knowledge(grandfather/2,[father/2,mother/2,husband/2,grandmother/2,parent/2]).
+% background_knowledge(grandfather/2,[father/2,mother/2,husband/2,grandmother/2,parent/2]).
 background_knowledge(grandmother/2,[father/2,mother/2,parent/2]).
 background_knowledge(parent/2,[father/2,mother/2]).
 background_knowledge(husband/2,[father/2,mother/2]).
 background_knowledge(wife/2,[father/2,mother/2]).
+%background_knowledge(child/2,[father/2,mother/2,parent/2]).
+background_knowledge(child/2,[male/1,female/1,son/2,daughter/2,father/2,mother/2,parent/2]).
+background_knowledge(son/2,[father/2,mother/2,parent/2,child/2,male/1]).
+background_knowledge(daughter/2,[father/2,mother/2,parent/2,child/2,female/1]).
 background_knowledge(father/2,[father/2,mother/2,parent/2,male/1]).
 background_knowledge(mother/2,[father/2,mother/2,parent/2,female/1]).
 background_knowledge(male/2,[male/1]).
@@ -43,6 +50,10 @@ metarules(grandmother/2,[chain]).
 metarules(parent/2,[identity]).
 metarules(husband/2,[switch]).
 metarules(wife/2,[switch]).
+%metarules(child/2,[inverse]).
+metarules(child/2,[identity]).
+metarules(son/2,[precon]).
+metarules(daughter/2,[precon]).
 metarules(father/2,[precon,projection]).
 metarules(mother/2,[precon,projection]).
 metarules(male/2,[identity,projection]).
@@ -63,6 +74,12 @@ positive_example(husband/2,husband(A,B)):-
 	husband(A,B).
 positive_example(wife/2,wife(A,B)):-
 	wife(A,B).
+positive_example(child/2,child(A,B)):-
+	child(A,B).
+positive_example(son/2,son(A,B)):-
+	son(A,B).
+positive_example(daughter/2,daughter(A,B)):-
+	daughter(A,B).
 positive_example(father/2,father(A,B)):-
 	father(A,B).
 positive_example(mother/2,mother(A,B)):-
@@ -82,6 +99,16 @@ negative_example(grandmother/2,grandmother(A,B)):-
 	grandfather(A,B).
 negative_example(parent/2,parent(A,B)):-
 	parent(B,A).
+negative_example(husband/2,husband(A,B)):-
+	wife(A,B).
+negative_example(wife/2,wife(A,B)):-
+	husband(A,B).
+negative_example(child/2,child(A,B)):-
+	parent(A,B).
+negative_example(son/2,son(A,B)):-
+	daughter(A,B).
+negative_example(daughter/2,daughter(A,B)):-
+	son(A,B).
 negative_example(father/2,father(A,B)):-
 	mother(A,B).
 negative_example(mother/2,mother(A,B)):-
@@ -90,10 +117,6 @@ negative_example(male/2,male(A,A)):-
 	female(A).
 negative_example(female/2,female(A,A)):-
 	male(A).
-negative_example(husband/2,husband(A,B)):-
-	wife(A,B).
-negative_example(wife/2,wife(A,B)):-
-	husband(A,B).
 
 
 % Background knowledge definitions
@@ -128,6 +151,24 @@ husband(X,Y):-
 wife(X,Y):-
 	mother(X,Z)
 	,father(Y,Z).
+
+child(X,Y):-
+	parent(Y,X).
+/*child(X,Y):-
+	son(X,Y).
+child(X,Y):-
+	daughter(X,Y).
+Causes learning child with son/2, daughter/2 and identity to go
+infinite!
+*/
+
+son(X,Y):-
+	male(X)
+	,child(X,Y).
+
+daughter(X,Y):-
+	female(X)
+	,child(X,Y).
 
 father(stathis, kostas).
 father(stefanos, dora).
