73210268a5fd8325258f760280a1430cfd3c4269
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-07-20 20:17:02 +0100

    Added learning rate experiment to kinship.pl

--- data/examples/kinship/kinship.pl
+++ data/examples/kinship/kinship.pl
@@ -20,6 +20,9 @@
 		  ,mother/2
 		  ,male/1
 		  ,female/1
+		  ,married/2
+		  ,learning_rate/2
+		  ,debug_learning_rate/1
 		  ]).
 
 
@@ -35,6 +38,135 @@ don't know how high resolutions/1 should go to fully reduce the theory.
 Bit of a weakness of the procedure there :/
 
 */
+
+/* ================================================================================
+   EXPERIMENT PREDICATES
+   ================================================================================
+*/
+
+% Comment out to disable debugging.
+:-debug(learning_rate).
+
+%!	debug_learning_rate(+Target) is det.
+%
+%	Debug a learning rate experiment.
+%
+%	Prints results of the experiment to the console.
+%
+debug_learning_rate(T):-
+	learning_rate(T,Rs)
+	,forall(member(N-S,Rs)
+	       ,format('Clauses: ~w Time: ~4f~n',[N,S])).
+
+
+%!	learning_rate(+Target,-Results) is det.
+%
+%	Perform	a learning rate experiment.
+%
+%	Target is the predicate indicator of the learning target. Result
+%	is a list of key-value pairs, N-T, where each key, N, is the
+%	size of a hypothesis of Target (in clauses) and each value, T,
+%	is the time it took to learn that hypothesis, measured in
+%	seconds.
+%
+%	The learning rate experiment performed by this predicate
+%	incrementally takes more of the target theory for Target and
+%	uses it to generate positive examples and find background
+%	knowledge for a training atempt. Background knowledge is found
+%	by taking the symbols and arities of body literals in all
+%	clauses of Target in the Target's definition in the current
+%	step.
+%
+learning_rate(T,Rs):-
+	program(T,kinship,Ts)
+	,experiment_data(T,_,Neg,_BK,MS)
+	,learning_rate([],Ts,Neg,MS,[],Rs_)
+	,findall(N-S
+		,(member(S-Ps,Rs_)
+		 ,length(Ps,N)
+		 )
+		,Rs).
+
+%!	learning_rate(+Current,+Theory,+Neg,+Metarules,+Acc,-Results) is
+%!	det.
+%
+%	Business end of learning_rate/2.
+%
+learning_rate(Ts_i,[],_Neg,_MS,Acc,Rs):-
+	Ts_i \= []
+	,reverse(Acc,Rs)
+	,!.
+learning_rate(Ts_i,[C|Ts],Neg,MS,Acc,Bind):-
+	theory_examples_bk([C|Ts_i],Pos,BK)
+	,G = learn(Pos,Neg,BK,MS,Ps)
+	,timing(G,T)
+	,!
+	,length(Ps,N)
+	,debug(learning_rate,'Learned a ~w-clause hypothesis.',[N])
+	,learning_rate([C|Ts_i],Ts,Neg,MS,[T-Ps|Acc],Bind).
+learning_rate(Ts_i,_Ts,Neg,MS,Acc,Bind):-
+	length(Ts_i,N)
+	,debug(learning_rate,'Learning failed after ~w clauses.',[N])
+	,learning_rate(Ts_i,[],Neg,MS,Acc,Bind).
+
+
+%!	theory_examples_bk(+Target,-Positive,-BK) is det.
+%
+%	Generate Positive examples and BK for Target.
+%
+theory_examples_bk(Ts,Pos_,BK):-
+	theory_symbols(Ts,BK)
+	,program(BK,kinship,BK_Ps)
+	,lfp(BK_Ps,BK_As)
+	,lfp_query(Ts,BK_As,_Is,Pos)
+	,sort(Pos, Pos_).
+
+
+%!	theory_symbols(+Theory,-Symbols) is det.
+%
+%	Collect the predicate Symbols of body literals in Theory.
+%
+theory_symbols(Ts,Ss):-
+	theory_symbols(Ts,[],Ss).
+
+%!	theory_symbols(+Theory,+Acc,-Symbols) is det.
+%
+%	Business end of theory_symbols/2
+%
+theory_symbols(Ts,Acc,Bind):-
+	findall(F/A
+	       ,(member(_H:-B,Ts)
+		,once(list_tree(Bs,B))
+		,member(P,Bs)
+		,functor(P,F,A)
+		)
+	       ,Ss)
+	% No symbols if Ts is a list of atoms only.
+	,Ss \= []
+	,!
+	,program(Ss,kinship,Ts_)
+	,theory_symbols(Ts_,[Ss|Acc],Bind).
+theory_symbols(_Ts,Acc,Ss):-
+	flatten(Acc,Ss_)
+	,sort(Ss_,Ss).
+
+
+%!	timing(+Goal,-Time) is det.
+%
+%	Call a Goal and report the Time it took.
+%
+timing(G, T):-
+	S is cputime
+	,call(G)
+	,E is cputime
+	,T is E - S.
+
+
+/* ================================================================================
+   TRAINING DATA
+   ================================================================================
+*/
+
 
 background_knowledge(blood_relative/2,[grandparent/2
 				      ,grandfather/2
@@ -56,7 +188,8 @@ background_knowledge(blood_relative/2,[grandparent/2
 				      ,female/1
 				      ]).
 
-background_knowledge(unrelated/2,[grandfather/2
+background_knowledge(unrelated/2,[grandparent/2
+				 ,grandfather/2
 				 ,grandmother/2
 				 ,parent/2
 				 ,aunt/2
@@ -73,6 +206,7 @@ background_knowledge(unrelated/2,[grandfather/2
 				 ,mother/2
 				 ,male/1
 				 ,female/1
+				 ,married/2
 				 ]).
 
 
@@ -203,3 +337,18 @@ female(gioula).
 female(ada).
 female(efi).
 female(stassa).
+
+% BK definitions - not blood relations.
+married(akis,efi).
+married(kostas,theodora).
+married(miltos,ada).
+married(stathis,alexandra).
+married(stefanos,voula).
+married(vassilis,gioula).
+% Extensional reflexion of the "married" relation
+married(ada,miltos).
+married(alexandra,stathis).
+married(efi,akis).
+married(gioula,vassilis).
+married(theodora,kostas).
+married(voula,stefanos).
