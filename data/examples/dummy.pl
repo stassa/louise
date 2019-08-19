d008cc165b2b85e8c1f0b4fad021eb831b662e63
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-08-19 16:11:24 +0300

    Added dummy experiment file that can be easily copy/pasted and mod'd.

---
+++ data/examples/dummy.pl
@@ -0,0 +1,12 @@
+:-module(dummy, [background_knowledge/2
+		,metarules/2
+		,positive_example/2
+		,negative_example/2]).
+
+background_knowledge(_, _).
+
+metarules(_,_).
+
+positive_example(_,_).
+
+negative_example(_,_).
