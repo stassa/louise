3a318e232856f39aa26650f599693ea706018267
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-08-22 21:38:41 +0300

    Added headless project load file (ommits documentation browser, IDE).

---
+++ load_headless.pl
@@ -0,0 +1,20 @@
+:-prolog_load_context(directory, Dir)
+,asserta(user:file_search_path(project_root, Dir)).
+
+user:file_search_path(src, project_root(src)).
+user:file_search_path(lib, project_root(lib)).
+user:file_search_path(data, project_root(data)).
+user:file_search_path(output, project_root(output)).
+
+:-use_module(configuration).
+:-use_module(src(louise)).
+:-use_module(src(auxiliaries)).
+:-use_module(lib(evaluation/evaluation)).
+
+%:-load_test_files([]).
+%:-run_tests.
+
+% Large data may require a larger stack.
+%:-set_prolog_stack(global, limit(2**9*10**6)).
+:-current_prolog_flag(stack_limit, V)
+ ,format('Global stack limit ~D~n',[V]).
