40cbf5cf924b9f2e6d493bbe86882eec8765f9bb
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-10-21 00:30:45 +0100

    Auxiliary predicates to list and reset configuration options. * The new predicate, reset_defaults/0 resets configuration options to   their default values, defined in the new file src/defaults.pl. Only   dynamic options that are possible to manipulate dynamically with   set_configuration_option/2 are reset this way, and only the ones   actually defined (or re-defined) in the configuration option, i.e. not   those in libraries. * list_config/0 and debug_config/0 print configuration options to user   output or to the current logging stream for some debug subject. * print_or_debug/3 is used by list_config/0 and debug_config/0 but it's   generally handy for diverting logging messages to the console or a log   file, or both at the same time, so I'm exporting it also. * list_problem_statistics/1 lists numbers of positive and negative   examples, BK and metarules in an initial MIL problem (i.e. before   metarule extension). I'm always doing that by hand and it's a bit   stupid.

--- src/auxiliaries.pl
+++ src/auxiliaries.pl
@@ -1,4 +1,8 @@
-:-module(auxiliaries, [list_problem_statistics/1
+:-module(auxiliaries, [reset_defaults/0
+		      ,list_config/0
+		      ,debug_config/1
+		      ,print_or_debug/3
+		      ,list_problem_statistics/1
 		      ,same_metarule/2
 		      ,set_configuration_option/2
 		      ,load_experiment_file/0
@@ -27,9 +31,131 @@
 :-user:use_module(lib(program_reduction/program_reduction)).
 :-user:use_module(lib(mathemancy/mathemancy)).
 :-use_module(src(mil_problem)).
+:-use_module(src(defaults)).
 
 /** <module> Auxiliary predicates.
 */
+
+
+%!	list_config is det.
+%
+%	Print configuration options to the console.
+%
+%	Only configuration options actually defined in the configuration
+%	module (i.e. not re-exported from other configuration files) are
+%	printed.
+%
+list_config:-
+	list_config(print,user_output).
+
+
+
+%!	debug_config(+Subject) is det.
+%
+%	Log configuration options to the debug stream for Subject.
+%
+%	Only configuration options actually defined in the configuration
+%	module (i.e. not re-exported from other configuration files) are
+%	logged.
+%
+debug_config(S):-
+	list_config(debug,S).
+
+
+%!	list_config(+Print_or_Debug,+Atom) is det.
+%
+%	Print or debug current configuration options.
+%
+list_config(T,S):-
+	module_property(configuration, exports(Es))
+	,findall(Opt_
+		,(member(F/A,Es)
+		 ,\+ memberchk(F, [metarule,metarule_constraints])
+		 ,functor(Opt,F,A)
+		 ,predicate_property(Opt, implementation_module(configuration))
+		 ,call(configuration:Opt)
+		 % Keep as list to sort by functor only
+		 % Standard order of terms sorts by arity also.
+		 ,Opt =.. Opt_
+		 )
+		,Opts)
+	,sort(1,@<,Opts, Opts_)
+	,forall(member(Opt, Opts_)
+	       ,(Opt_ =.. Opt
+		,print_or_debug(T,S,Opt_)
+		)
+	       ).
+
+
+%!	print_or_debug(+Print_or_Debug,+Stream_or_Subject,+Atom) is
+%!	det.
+%
+%	Print or debug an Atom.
+%
+%	Print_or_Debug can be one of: [print,debug,both]. If "print",
+%	Stream_or_Subject should be the name or alias of a stream
+%	and Atom is printed at that Stream. If "debug",
+%	Stream_or_Subject should be a debug subject and Atom is printed
+%	to the current debug stream, iff the specified subject is being
+%	debugged. If "both", Stream_or_Subject should be a term Str/Sub,
+%	where Str the name or alias of a stream and Sub the name of
+%	debug topic; then Atom is printed to the specified stream and
+%	also to the current debug topic if Sub is being debugged.
+%
+print_or_debug(debug,S,C):-
+	debug(S,'~w',[C]).
+print_or_debug(print,S,C):-
+	format(S,'~w~n',[C]).
+print_or_debug(both,Str/Sub,C):-
+	print_or_debug(print,Str,C)
+	,print_or_debug(debug,Sub,C).
+
+
+
+%!	reset_defaults is det.
+%
+%	Reset dynamic configuration options to their defaults.
+%
+%	This predicate should be called after dynamic configuration
+%	options are changed dynamically with set_configuration_option/2
+%	to reset the changed options to their defaults.
+%
+%	Defaults of dynamic configuration options are stored in the file
+%	src(defaults).
+%
+%	Note that only options declared dynamic and whose implementation
+%	module is configuration are reset.
+%
+%	Some options may be originally implemented in a different
+%	module, but the configuration module may re-export them. Such
+%	options are _not_ rest.
+%
+%	Some options might be originally implemented in a different
+%	module, but the configuration module re-defines them and
+%	excludes them from its re-export list. For example, that is the
+%	case with the resolutions/1 option, originally defined in
+%	reduction_configuration.pl and declared dynamic. Such options
+%	_are_ reset to their defaults.
+%
+%	Consult the default.pl file for a definitive source of the
+%	options that should be reset by this predicate.
+%
+reset_defaults:-
+	module_property(configuration, exports(Es))
+	,forall(member(F/A, Es)
+	       ,(functor(P,F,A)
+		,(  \+ memberchk(F, [metarule,metarule_constraints])
+		   ,predicate_property(P, dynamic))
+		   ,predicate_property(P, implementation_module(configuration))
+		->  atom_concat(default_,F,DF)
+		   ,functor(D,DF,1)
+		   ,call(defaults:D)
+		   ,D =.. [_|Vs]
+		   ,set_configuration_option(F,Vs)
+		;  true
+		)
+	       ).
+
 
 
 %!	list_problem_statistics(+Target) is det.
---
+++ src/defaults.pl
@@ -0,0 +1,78 @@
+:-module(defaults, [default_extend_metarules/1
+		   ,default_max_invented/1
+		   ,default_recursion_depth_limit/1
+		   ,default_recursive_reduction/1
+		   ,default_reduction/1
+		   ,default_resolutions/1
+		   ,default_theorem_prover/1
+		   ]).
+
+/** <module> Default values for dynamic configuration options.
+
+Predicates in this module store the default values for configuration
+options that are declared dynamic so that they can be manipulated by
+set_configuration_option/2. The auxilary predicate reset_defaults/0 sets
+dynamic configuration options to the values specified in this file.
+*/
+
+%!	default_extend_metarules(?Default) is semidet.
+%
+%	Default value for extended_metarules/1 option.
+%
+%	The default value is "false".
+%
+default_extend_metarules(false).
+
+
+%!	default_max_invented(?Default) is semidet.
+%
+%	Default value for max_invented/1 option.
+%
+%	The default value is "1".
+%
+default_max_invented(1).
+
+
+%!	default_recursion_depth_limit(?Default) is semidet.
+%
+%	Default value for recursion_depth_limit/1 option.
+%
+%	The default value is "(episodic_learning, 100)".
+%
+default_recursion_depth_limit([episodic_learning, 100]).
+
+
+%!	default_recursive_reduction(?Default) is semidet.
+%
+%	Default value for recursive_reduction/1 option.
+%
+%	The default value is "false".
+%
+default_recursive_reduction(false).
+
+
+%!	default_reduction(?Default) is semidet.
+%
+%	Default value for reduction/1 option.
+%
+%	The default value is "plotkins".
+%
+default_reduction(plotkins).
+
+
+%!	default_resolutions(?Default) is semidet.
+%
+%	Default value for resolutions/1 option.
+%
+%	The default value is "5000".
+%
+default_resolutions(5000).
+
+
+%!	default_theorem_prover(?Default) is semidet.
+%
+%	Default value for theorem_prover/1 option.
+%
+%	The default value is "resolution".
+%
+default_theorem_prover(resolution).
