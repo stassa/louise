6e8d3586b1437bd3672d4dc75329bf6a892bd000
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-11-08 20:33:21 +0000

    Added new module, metarule_extraction to perform, well, guess. * Predicates in the new module extract metarules from the structure of   definite programs, by variabilisation. In particular, the user can   extract metarules from the definitions of background knowledge   predicates in an experiment file. The idea is that background   knowledge definitions are likely to have clauses with structures   relevant to the learning task. Note that metarules are only extracted   from intensionally defined background knowledge. * The idea for this module is totally lifted off MOBAL's Model   Acquisition Tool (MAT).

---
+++ lib/lifting/lifting.pl
@@ -0,0 +1,227 @@
+:-module(lifting, [lifted_program/2
+		  ]).
+
+/** <module> Predicates to replace terms with numbered variables.
+
+*/
+
+:-dynamic term_var/2.
+
+/* Helps debugging numbered vars.
+:- current_prolog_flag(debugger_write_options, X)
+  ,set_prolog_flag(debugger_write_options,[numbervars(false)|X]).
+*/
+
+%!	lifted_program(+P, -L) is det.
+%
+%	Replace constants in clauses of P with variables.
+%
+%	Best explained with an example:
+%	==
+%	?- P = [q(a,b), r(b,a), (p(X,Y) :- q(X, Z), r(Z,Y))],
+%	    lifting:lifted_program(P, P_),
+%	    numbervars(P_),
+%	    forall(member(C, P_),(write_canonical(C),nl)).
+%
+%	q('$VAR'(0),'$VAR'(1))
+%	r('$VAR'(1),'$VAR'(0))
+%	:-(p('$VAR'(2),'$VAR'(3)),','(q('$VAR'(2),'$VAR'(4)),r('$VAR'(4),'$VAR'(3))))
+%	P = [q(a, b), r(b, a),  (p(C, D):-q(C, E), r(E, D))],
+%	X = C,
+%	Y = D,
+%	Z = E,
+%	P_ = [q(A, B), r(B, A),  (p(C, D):-q(C, E), r(E, D))].
+%	==
+%
+%	Note that variables are also replaced with new variables.
+%
+%	@tbd This is the result of the use of varnumbers/2 to replace
+%	skolemised terms with fresh variables. It might be preferrable
+%	to leave existing variables untouched. That means writing a new
+%	varnumbers/2 ensuring this.
+%
+lifted_program(P, P_):-
+	lifted_clauses(-1, P, [], P1)
+	,varnumbers(P1,P_)
+	,retractall(term_var(_,_)).
+
+
+%!	lifted_clauses(+I,+Cs,+Acc,-Bind) is det.
+%
+%	Business end of lifted_program/2.
+%
+%	Lifts each clause in the list of clauses Cs.
+%
+%	I is an index of the last variable number assigned to a term in
+%	a clause in Cs, used to maintain consistency.
+%
+lifted_clauses(_, [], Cs, Cs_):-
+	reverse(Cs, Cs_)
+	,!.
+lifted_clauses(I,[C|Cs],Acc,Bind):-
+	lifted_clause(I,J,C,C_)
+	,lifted_clauses(J,Cs,[C_|Acc],Bind).
+
+
+%!	lifted_clause(+I,-K,+C,-C_lifted) is det.
+%
+%	Business end of lifted_clauses/4.
+%
+%	Lifts each literal in clause C.
+%
+%	I is the last variable number assigned to a literal before the
+%	first literal in C; K is the last variable number assigned to a
+%	literal by this predicate.
+%
+lifted_clause(I, K, (H:-B), (H_:-B_)):-
+	!
+	,lifted_literal(I,J,H,H_)
+	,lifted_literals(J,K,B,(H_),Ls)
+	,treeverse(Ls, (H_,B_)).
+lifted_clause(I, J, (L), L_):-
+	lifted_literal(I, J, L, L_).
+
+
+%!	lifted_literals(+I,-K,+Ls,+Acc,-Bind) is det.
+%
+%	Auxiliary to lifted_clause/4.
+%
+%	The purpose of this is mainly to allow construction of the list
+%	of lifted literals without excess use of reversing operations.
+%
+%	@tbd Basically, this exists because we want to initialise the
+%	accumulator variable in lifted_clause/4 with the first literal
+%	in a clause, instead of with an empty variable, because that
+%	leaves an empty variable at the tail of the accumulator. It's
+%	probably possible to do this in lifted_clause/4 itself and avoid
+%	the complication of additional predicates.
+%
+lifted_literals(I, K, (L,Ls), Acc, Bind):-
+	!
+	,lifted_literal(I, J, L, L_)
+	,lifted_literals(J, K, Ls, (L_,Acc), Bind).
+lifted_literals(I, J, (L), Acc, (L_,Acc)):-
+	lifted_literal(I, J, L, L_).
+
+
+%!	lifted_literal(+I, -J, +L, -Li) is det.
+%
+%	Lift a single literal.
+%
+%	I is the number of the last variable assigned to a term in a
+%	literal before L. J is the number of the last variable assigned
+%	to a term in L.
+%
+%	Examples:
+%	==
+%	?- lifting:lifted_literal(-1,J,p(a,b(c,X,d),e),Vs),write_canonical(Vs).
+%	p('$VAR'(0),b('$VAR'(1),_,'$VAR'(2)),'$VAR'(3))
+%	J = 2,
+%	Vs = p(A, b(B, X, C), D).
+%
+%	?- lifting:lifted_literal(0, J, p(a,b(c,X,d),e), Vs),write_canonical(Vs).
+%	p('$VAR'(1),b('$VAR'(2),_,'$VAR'(3)),'$VAR'(4))
+%	J = 3,
+%	Vs = p(B, b(C, X, D), E).
+%	==
+%
+lifted_literal(I, J, L, L_):-
+	L =.. [F|As]
+	,lifted_terms(I, J, As, As_)
+	,L_ =.. [F|As_].
+
+
+%!	lifted_terms(+I, -J, +Ts, -Ls) is det.
+%
+%	Business end of lifted_literal/4.
+%
+%	Lift each term in a literal. Ts is a list of terms in the
+%	argument list of a literal to be lifted. I, J index the last
+%	numbered variables in previous literals and the parent literal
+%	of Ts.
+%
+lifted_terms(I, J, As, As_):-
+	lifted_terms(I, J, As, [], As_).
+
+
+%!	lifted_terms(+I, -J, +Ts, +Acc, -Bind) is det.
+%
+%	Business end of lifted_terms/4.
+%
+%	Lifts each term in Ts by replacing it with a numbered variable,
+%	making sure that the same number is associated with the same
+%	term always. Ts is a list of terms in the argument list of a
+%	literal to be lifted. I, J index the last terms numbered for
+%	previous literals or the parent literal of Ts.
+%
+%	Example:
+%	==
+%	?- _P = [(enc(p,X,Y):-enc(q,Y,X)), enc(r,X,Z) ], lifted_program(_P, L).
+%	X = B,
+%	Y = C,
+%	Z = F,
+%	L = [(enc(A, B, C):-enc(D, C, B)), enc(E, B, F)].
+%	==
+%
+lifted_terms(I, I, [], Ts, Ts_):-
+	reverse(Ts, Ts_)
+	,!.
+lifted_terms(I,J,[T|Ts],Acc,Bind):-
+	compound(T)
+	,T = '$VAR'(K)
+	,!
+	,lifted_terms(I,J,Ts,['$VAR'(K)|Acc],Bind).
+lifted_terms(I, J, [T|Ts], Acc, Bind):-
+	compound(T)
+	,!
+	,T =.. [F|As]
+	,lifted_terms(I,J,As,[],As_)
+	,T_ =.. [F|As_]
+	,lifted_terms(J,_,Ts, [T_|Acc], Bind).
+lifted_terms(I, J, [T|Ts], Acc, Bind):-
+	var(T)
+	,increment(I,I_)
+	,T = '$VAR'(I_)
+	,!
+	,lifted_terms(I_, J, Ts, [T|Acc], Bind).
+lifted_terms(I,J,[T|Ts],Acc,Bind):-
+	term_var(T, K)
+	,!
+	,lifted_terms(I, J, Ts, ['$VAR'(K)|Acc], Bind).
+lifted_terms(I, J, [T|Ts], Acc, Bind):-
+	increment(I, I_)
+	,asserta(term_var(T, I_))
+	,lifted_terms(I_, J, Ts, ['$VAR'(I_)|Acc], Bind).
+
+
+
+%!	increment(+I, -Ipp) is det.
+%
+%	Increment I by one.
+%
+%	Simple wrapper around succ/2. Because succ/2 doesn't deal with
+%	negatives (and we want to start counting from -1 for numbered
+%	vars).
+%
+increment(-1, 0):-
+	!.
+increment(I, I_):-
+	succ(I, I_).
+
+
+
+%!	treeverse(+Tree, Eert) is det.
+%
+%	Like reverse/2 but for trees.
+%
+%	Alternatively, a universe of trees. Free hugs. No squares
+%	allowed.
+%
+treeverse((X,Xs),Ys):-
+	treeverse(Xs,(X),Ys).
+
+treeverse((X,Xs),Acc,Bind):-
+	!
+	,treeverse(Xs,(X,Acc),Bind).
+treeverse(X,Ys,(X,Ys)).
+
---
+++ src/metarule_extraction.pl
@@ -0,0 +1,244 @@
+:-module(metarule_extraction, [bk_metarules/2
+			      ,symbols_metarules/3
+			      ,program_metarules/2
+			      ]).
+
+:-use_module(src(auxiliaries)).
+:-use_module(src(mil_problem)).
+:-use_module(lib(lifting/lifting)).
+
+
+/** <module> Extract metarules from first-order background knowledge.
+
+Predicates in this module extract metarules from a definite program by
+variabilising the program's clauses. Predicate symbols in the program's
+clauses are replaced with existentially quantified, second-order
+variables and constants are replaced with existentially or universally
+quantified first order variables. Identical terms are replaced with the
+same variable and any variables pre-existing in a clauses are retained
+unchanged.
+
+Note that, since all this is done in Prolog that has no concept of
+existentially quantified variables or second-order terms, in practice
+all variables in the output of predicates in this module are first-order
+and (implicitly) universally quantified. That makes no difference in how
+metarules are actually used.
+
+A notable use of predicates in this module is to extract metarules from
+the definitions of predicates declared as background knowledge for a
+learning target.
+
+Examples
+========
+
+1. program_metarules/2 is used to xtract metarules from an arbitrary
+list of definite clauses:
+
+==
+?- _Ps = [ p(a,_Y):- q(a,_Z), r(_Z,_Y), p(a,b) ]
+  ,program_metarules(_Ps, _MS), print_clauses(_MS).
+
+m(metarule_1,A,B):-m(A,C,D),m(B,C,E).
+true.
+==
+
+2. symbols_metarules/r is used to extract metarules from a list of
+predicate symbols and arities. The second argument is the name of a
+module where the given predicates are defined, or a module importing
+those predicates' definition module:
+
+==
+?- symbols_metarules([symbols_metarules/3], user, _MS)
+  ,print_clauses(_MS).
+
+m(metarule_1,A,B,C):-m(A,D,E,F),m(B,D,E,G),m(C,G,F).
+true.
+
+% Definition of symbols_metarules/3 in this file:
+% symbols_metarules(Ss,M,MS):-
+%	program(Ss, M, Ps)
+%	,program_metarules(Ps, MS).
+==
+
+3. bk_metarules/2 is used to extract metarules from the definitions of
+predicates declared as background for a learning target. Below,
+metarules are extracted from the definitions of background predicates
+for grandfather/2, one of the learning targets in
+data/examples/tiny_kinship.pl (its BK predicates are also defined in
+the same module):
+
+==
+?- bk_metarules(grandfather/2, _MS), print_clauses(_MS).
+m(metarule_1,A,B):-m(A,C,D),m(B,C,D).
+m(metarule_2,A,B,C):-m(A,D,E),m(B,D,F),m(C,E,F).
+m(metarule_3,A,B,C):-m(A,D,E),m(B,D,F),m(C,F,E).
+true.
+
+% BK declaration for grandfather/2 in examples/tiny_kinship.pl:
+% background_knowledge(grandfather/2,[father/2,parent/2,husband/2,grandmother/2]).
+
+% Definitions of BK predicates for grandfather/2 in tiny_kinship.pl):
+% parent(X, Y):- father(X,Y).
+% parent(X, Y):- mother(X,Y).
+%
+% husband(X,Y):- father(X,Z) ,mother(Y,Z).
+% grandmother(A,B):- mother(A,C) ,parent(C,B).
+==
+
+In the examples above, note that metarules are given generic names
+consisting of the constant "metarule", followed by an underscore and a
+numeric index, indicating the order of the metarule in the list of
+metarules output by bk_metarules/2. There is no guarantee that such
+metarule IDs will be in any way unique: the user is required to give
+them unique names, perhaps evern meaningful names.
+*/
+
+
+%!	bk_metarules(+Target,-Metarules) is det.
+%
+%	Extract Metarules from background knowledge declared for Target.
+%
+%	Target is a learning target defined in the current experiment
+%	file. Metarule is a list of metarule extracted from the
+%	definitions of the predicates declared as background knowledge
+%	for Target in the current experiment file.
+%
+%	Metarules are extracted by a call to symbols_metarules/3,
+%	passing the predicate symbols of the background predicates, and
+%	the module 'user'. Accordingly, only predicates defined in the
+%	current experiment file and exported to module user will be used
+%	to extract metarules.
+%
+bk_metarules(T, MS):-
+	experiment_data(T,_Pos,_Neg,BK,_MS)
+	,symbols_metarules(BK,user,MS).
+
+
+%!	symbols_metarules(+Symbols,+Module,-Metarules) is det.
+%
+%	Extract a list of Metarules given a list of predicate Symbols.
+%
+%	Symbols is a list of predicate indicators, the symbols and
+%	arities of predicates in a program from which mearules are to be
+%	extracted. The program must be visible to the given Module (i.e.
+%	either defined in that Module, or imported to it from another
+%	module).
+%
+%	Metarules is a list of metarules extracted from the program
+%	indicated by the predicate indicators in Symbols, by a call to
+%	program_metarules/2.
+%
+symbols_metarules(Ss,M,MS):-
+	program(Ss, M, Ps)
+	,program_metarules(Ps, MS).
+
+
+%!	program_metarules(+Program, -Metarules) is det.
+%
+%	Extract a list of Metarules from a first order Program.
+%
+%	Program is a list of first-order definite clauses. Metarules is
+%	a list of metarules such that each non-unit clause in Program is
+%	an instance of a Metarule in Metarules.
+%
+%	Metarules in the output list are given generic metarule IDs,
+%	consisting of the constant "metarule" followed by an underscore
+%	and a number, the position of the metarule in the list,
+%	Metarules. These are by no means guaranteed to be unique and
+%	it is expected taht the user will inspect the metarules
+%	resulting from a call to this predicate and assign unique names
+%	to the ones she wants to keep, perhaps even meaningful names if
+%	that is at all possible.
+%
+%	@tbd Note that the description above means that Metarules
+%	are not extracted from unit clauses. Such metarules are usually
+%	of the form m(P,A,B) and not particularly useful in practice.
+%
+program_metarules(Ps, MS):-
+	encapsulated_clauses(Ps, Es)
+	,lifted_program(Es,Ls)
+	,setof(A:-(H,B)
+	      ,Ls^H^Exs^(member(H:-B,Ls)
+			,numbervars(H:-B)
+			,existential((H,B),Exs)
+			,metasubs_atom(Exs,A)
+			)
+	      ,CS_)
+	,varnumbers(CS_,MS_)
+	,numbered_metarules(MS_,MS).
+
+
+%!	existential(+Literals, -Variables) is det.
+%
+%	Extract the existentially quantified Variables of Literals.
+%
+%	Literals is a set of literals of a metarule. Variables are the
+%	second-order existentially quantified variables in each of its
+%	literals.
+%
+existential(Ls,Es):-
+	existential(Ls, [], Es).
+
+%!	existential(+Literal,+Acc,-Variables) is det.
+%
+%	Business end of existential/2.
+%
+existential(L,Acc,Es):-
+	L \= (_,_)
+	,!
+	,L =.. [m,P|_Vs]
+	,reverse([P|Acc],Es).
+existential((L,Ls),Acc,Bind):-
+	L =.. [m,P|_Vs]
+	,existential(Ls,[P|Acc],Bind).
+
+
+%!	metasubs_atom(+Existential, -Atom) is det.
+%
+%	Constructe a metasubstitution Atom for a metarule.
+%
+%	Existential is the list of second-order existentially quantified
+%	variables in metarule. Atom is a metasubstitution atom of the
+%	form: m(,metarule,E1,...,En) where each Ei is a variable in
+%	Existential.
+%
+%	@tbd Metasubstitution atoms are assigned the generic metarule
+%	Id, "metarule". This is later indexed by a number relative to
+%	the number of metarules extracted from a program by a call to
+%	numbered_metarules/2.
+%
+metasubs_atom(Es,A):-
+	A =.. [m,metarule|Es].
+
+
+%!	numbered_metarules(+Metarules,-Numbered) is det.
+%
+%	Give a list of Metarules Numbered identifiers.
+%
+%	Metarules is a list of expanded metarules, with metasubstitution
+%	atoms each having the generic Id "metarule". Numbered is the
+%	same list of metarules where the generic "metarule" Id is
+%	indexed with a number, the order of the mearule in the list
+%	Metarules.
+%
+%	@tbd This predicate is called at the end of processing in
+%	program_metarules/2 when all metarules that can be extracted
+%	from an input program have been extracted and then sorted to
+%	remove duplicates. As a result, numeric indices appended to
+%	mearule Ids in Numbered begin at 1 and are unique with respect
+%	to the set of metarules in the input program. However, there is
+%	no guarantee that each indexed metarule Id will be unique across
+%	a project, leta lone multiple extraction runs.
+%
+numbered_metarules(MS,MS_):-
+	C = c(1)
+	,findall(A_:-M
+	       ,(member(A:-M,MS)
+		,A =.. [m,ID|As]
+		,arg(1,C,I)
+		,atomic_list_concat([ID,I],'_',ID_I)
+		,A_ =.. [m,ID_I|As]
+		,succ(I,I_)
+		,nb_setarg(1,C,I_)
+		)
+	       ,MS_).
