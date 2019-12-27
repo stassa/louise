c23256d9403f8398622d77ee2179ad11b502545d
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-12-27 16:07:42 +0000

    Added the new scripts directory to the headless project load file.

--- load_headless.pl
+++ load_headless.pl
@@ -5,6 +5,7 @@ user:file_search_path(src, project_root(src)).
 user:file_search_path(lib, project_root(lib)).
 user:file_search_path(data, project_root(data)).
 user:file_search_path(output, project_root(output)).
+user:file_search_path(scripts, project_root(scripts)).
 
 :-use_module(configuration).
 :-use_module(src(louise)).
