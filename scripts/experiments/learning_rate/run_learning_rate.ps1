e6ca9f4e9b6f16a01097352d590d0aedea1b6696
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2019-12-30 14:55:17 +0000

    Added run scripts for learning rate experiment. * These should really not be in Louise- move all this stuff to a   separate repository. Would make lots more sense.

---
+++ scripts/experiments/learning_rate/run_learning_rate.ps1
@@ -0,0 +1,3 @@
+& 'C:\Program Files\swipl\bin\swipl.exe' -s scripts/experiments/learning_rate/run_learning_rate.pl -g run_kin -t halt
+& 'C:\Program Files\swipl\bin\swipl.exe' -s scripts/experiments/learning_rate/run_learning_rate.pl -g run_mtg_fragment -t halt
+& 'C:\Program Files\swipl\bin\swipl.exe' -s scripts/experiments/learning_rate/run_learning_rate.pl -g run_robots -t halt
