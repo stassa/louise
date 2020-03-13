484d18504321c921165da8604ecc45dfd8989171
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2020-03-13 23:39:27 +0000

    Made configuration option minimal_program_size/2 dynamic. * I can totally see that as an option that may need to be manipulated   dynamically.

--- configuration.pl
+++ configuration.pl
@@ -27,6 +27,7 @@
 % Dynamic configuration options can be manipulated
 % by means of set_configuration_option/2 in module auxiliaries.
 :- dynamic max_invented/1
+          ,minimal_program_size/2
           ,recursion_depth_limit/2
 	  ,recursive_reduction/1
 	  ,reduction/1
