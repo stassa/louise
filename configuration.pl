66ce104bd1c5247a7e38b65a0b55c4785fa8705d
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2021-10-30 14:26:58 +0300

    Added thelma config option order_constraints/5 to main configuration.

--- configuration.pl
+++ configuration.pl
@@ -10,6 +10,7 @@
 			,metarule/2
 			,metarule_constraints/2
                         ,metarule_learning_limits/1
+                        ,order_constraints/5
 			,recursion_depth_limit/2
 			,recursive_reduction/1
                         ,reduce_learned_metarules/1
@@ -30,7 +31,10 @@
 	   except([resolutions/1])).
 :-reexport(lib(evaluation/evaluation_configuration)).
 :-reexport(lib/sampling/sampling_configuration).
-:-reexport(lib/thelma/thelma_configuration, except([depth_limits/2])).
+:-reexport(lib/thelma/thelma_configuration
+          ,except([depth_limits/2
+                  ,order_constraints/5
+                  ])).
 
 % Dynamic configuration options can be manipulated
 % by means of set_configuration_option/2 in module auxiliaries.
@@ -109,7 +113,7 @@ example_clauses(call).
 %       If Invented is not at most 1 lower than Clauses, it will be
 %       treated as being one lower than Clauses.
 %
-depth_limits(2,0).
+depth_limits(2,1).
 
 
 %!      generalise_learned_metarules(?Bool) is semidet.
@@ -466,6 +470,87 @@ metarule_learning_limits(none).
 %metarule_learning_limits(coverset).
 %metarule_learning_limits(sampling(0.5)).
 %metarule_learning_limits(metasubstitutions(1)).
+
+
+%!      order_constraints(+Id,+Existential,+FO,+Lexicographic,+Interval)
+%       is semidet.
+%
+%	Metarule order constraints for Thelma.
+%
+%       Id is an atom, the id of a metarule for which these constraints
+%       are defined.
+%
+%       Existential is the list of existentially quantified variables in
+%       the identified metarule. This list must include both
+%       second-order and first-order existentially quantified variables.
+%
+%       FO is the list of first-order variables in the identified
+%       metarule. This list must include both universally and
+%       existentially quantified first-order variables.
+%
+%       Lexicographic is a list of lexicographic order constraints over
+%       the second-order varibles in Existential. Lexicographic order
+%       constraints are defined as pairs P>Q where P, Q are second-order
+%       variables included in the list Existentials. The meaning of P>Q
+%       is that any predicate symbol bound to P during learning is above
+%       any predicate symbol bound to Q, in the lexicographic order of
+%       predicate symbols.
+%
+%       Interval is a list of interval order constraints over the
+%       first-order variables in a metarule. Interval order constraints
+%       are defined as pairs X>Y, with the same meaning as for
+%       lexicographic order constraints, except here X and Y are
+%       first-order variables that bind to constants, rather than
+%       predicate symbols, during learning.
+%
+%       __Motivation__
+%
+%       Order constraints are used in the originally described version
+%       of Metagol to ensure termination when learning recursion. They
+%       impose a total ordering over the Herbrand base used to restrict
+%       the instantiations of variables in metarules.
+%
+%       Lexicographic order constraints ensure termination of the
+%       learning procedure when learning recursive hypotheses. Interval
+%       order constraints are necessary when hypotheses include
+%       mutually recursive predicates, most notably when an auxiliary
+%       predicate that is mutually recursive with the target predicate
+%       must be invented in order to learn the target.
+%
+%       Lexicographic and interval order constraints are more fully
+%       explained in the following reference:
+%       ==
+%       Meta-interpretive learning of higher-order dyadic datalog:
+%       predicate invention revisited
+%       Mach Learning (2015) 100:49-73
+%       Muggleton, et al.
+%       ==
+%
+%       @tbd Order constraints require a total ordering over the
+%       predicate symbols and constants in a learning problem. Such an
+%       ordering is derived automatically from the order in which
+%       background predicates' definitions and examples are declared in
+%       an experiment file. This is achieved by a call to
+%       order_constraints/3. See that predicate for more information,
+%       including the ability to manually defined a total ordering.
+%       Ouch.
+%
+%       @bug Currently, only lexicographic order constraints are
+%       implemented in Thelma. Defining interval order constraints won't
+%       have any effect at all unless this is resolved.
+%
+order_constraints(unit,_Ss,_Fs,[],[]).
+order_constraints(projection_21,[P,Q],_Fs,[P>Q],[]).
+order_constraints(projection_12,[P,Q],_Fs,[P>Q],[]).
+order_constraints(inverse,[P,Q],_Fs,[P>Q],[]).
+order_constraints(identity,[P,Q],_Fs,[P>Q],[]).
+order_constraints(chain,[P,Q,R],_Fs,[P>Q,P>R],[]).
+% One less constraint - for anbn.pl
+%order_constraints(chain,[P,Q,_R],_Fs,[P>Q],[]).
+order_constraints(tailrec,[P,Q],[X,Y,Z],[P>Q],[X>Z,Z>Y]).
+order_constraints(precon,[P,Q,R],_Fs,[P>Q,P>R],[]).
+order_constraints(postcon,[P,Q,R],_Fs,[P>Q,P>R],[]).
+order_constraints(switch,[P,Q,R],_Fs,[P>Q,P>R],[]).
 
 
 %!	recursion_depth_limit(?Purpose,?Limit) is semidet.
--- lib/thelma/thelma_configuration.pl
+++ lib/thelma/thelma_configuration.pl
@@ -35,28 +35,107 @@ default_ordering(lower).
 depth_limits(2,1).
 
 
-%!	order_constraints(+M,+Second_Order,+First_Order,+SO_Constraints,+FO_Constraints)
-%!	is det.
+%!      order_constraints(+Id,+Existential,+FO,+Lexicographic,+Interval)
+%       is semidet.
 %
-%	A set of order constraints for a metarule, M.
+%	Metarule order constraints for Thelma.
 %
-%	Lists order constraints for commonly used metarules. Like
-%	metarule/4, this predicate is also marked as multifile so
-%	experiment files can declare their own special order
-%	constraints. See notes in metarule/4.
+%       Id is an atom, the id of a metarule for which these constraints
+%       are defined.
+%
+%       Existential is the list of existentially quantified variables in
+%       the identified metarule. This list must include both
+%       second-order and first-order existentially quantified variables.
+%
+%       FO is the list of first-order variables in the identified
+%       metarule. This list must include both universally and
+%       existentially quantified first-order variables.
+%
+%       Lexicographic is a list of lexicographic order constraints over
+%       the second-order varibles in Existential. Lexicographic order
+%       constraints are defined as pairs P>Q where P, Q are second-order
+%       variables included in the list Existentials. The meaning of P>Q
+%       is that any predicate symbol bound to P during learning is above
+%       any predicate symbol bound to Q, in the lexicographic order of
+%       predicate symbols.
+%
+%       Interval is a list of interval order constraints over the
+%       first-order variables in a metarule. Interval order constraints
+%       are defined as pairs X>Y, with the same meaning as for
+%       lexicographic order constraints, except here X and Y are
+%       first-order variables that bind to constants, rather than
+%       predicate symbols, during learning.
+%
+%       __Motivation__
+%
+%       Order constraints are used in the originally described version
+%       of Metagol to ensure termination when learning recursion. They
+%       impose a total ordering over the Herbrand base used to restrict
+%       the instantiations of variables in metarules.
+%
+%       Lexicographic order constraints ensure termination of the
+%       learning procedure when learning recursive hypotheses. Interval
+%       order constraints are necessary when hypotheses include
+%       mutually recursive predicates, most notably when an auxiliary
+%       predicate that is mutually recursive with the target predicate
+%       must be invented in order to learn the target.
+%
+%       Lexicographic and interval order constraints are more fully
+%       explained in the following reference:
+%       ==
+%       Meta-interpretive learning of higher-order dyadic datalog:
+%       predicate invention revisited
+%       Mach Learning (2015) 100:49-73
+%       Muggleton, et al.
+%       ==
+%
+%       @tbd Order constraints require a total ordering over the
+%       predicate symbols and constants in a learning problem. Such an
+%       ordering is derived automatically from the order in which
+%       background predicates' definitions and examples are declared in
+%       an experiment file. This is achieved by a call to
+%       order_constraints/3. See that predicate for more information,
+%       including the ability to manually defined a total ordering.
+%       Ouch.
+%
+%       @bug Currently, only lexicographic order constraints are
+%       implemented in Thelma. Defining interval order constraints won't
+%       have any effect at all unless this is resolved.
 %
 order_constraints(unit,_Ss,_Fs,[],[]).
 order_constraints(projection_21,[P,Q],_Fs,[P>Q],[]).
 order_constraints(projection_12,[P,Q],_Fs,[P>Q],[]).
 order_constraints(inverse,[P,Q],_Fs,[P>Q],[]).
 order_constraints(identity,[P,Q],_Fs,[P>Q],[]).
-order_constraints(chain,[P,Q,R],_Fs,[P>Q,P>R],[]).
+%order_constraints(chain,[P,Q,R],_Fs,[P>Q,P>R],[]).
 % One less constraint - for anbn.pl
-%order_constraints(chain,[P,Q,_R],_Fs,[P>Q],[]).
+order_constraints(chain,[P,Q,_R],_Fs,[P>Q],[]).
 order_constraints(tailrec,[P,Q],[X,Y,Z],[P>Q],[X>Z,Z>Y]).
 order_constraints(precon,[P,Q,R],_Fs,[P>Q,P>R],[]).
 order_constraints(postcon,[P,Q,R],_Fs,[P>Q,P>R],[]).
 order_constraints(switch,[P,Q,R],_Fs,[P>Q,P>R],[]).
+
+% data/examples/yamamoto.pl
+order_constraints(postcon_unit,[P,Q,_R],_Fs,[P>Q],[]).
+
+% data/drafts/one_shot/nat.pl
+%order_constraints(m1,[P,Q],_Fs,[P>Q],[]).
+%order_constraints(m2,[P,Q,_R],_Fs,[P>Q],[]).
+
+% data/drafts/one_shot/even_odd.pl
+order_constraints(m1,[P,Q],_Fs,[P>Q],[]).
+order_constraints(m2,[P,Q,_R],_Fs,[P>Q],[]).
+
+% data/drafts/one_shot/findlast.pl
+%order_constraints(postcon1,[P,Q,_R],_Fs,[P>Q],[]).
+%order_constraints(precon1,[P,Q,_R],_Fs,[P>Q],[]).
+
+% data/drafts/one_shot/findlast_acyclic.pl
+%order_constraints(tailrec1,[P,Q],_Fs,[P>Q],[]).
+%order_constraints(midcon,[P,Q,_R,_S],_Fs,[P>Q],[]).
+
+% Catchall constraint.
+%order_constraints(_,_Ss,_Fs,[],[]).
 
 
 %!	metarule_functor(?Functor) is semidet.
