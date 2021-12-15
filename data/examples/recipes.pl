e223e218cabc1a191a7193ccbce00b82771098b7
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2021-12-15 19:12:52 +0200

    Updated recipe/2 to show new predicate invention and unfolding.

--- data/examples/recipes.pl
+++ data/examples/recipes.pl
@@ -14,33 +14,242 @@
 
 :-use_module(configuration).
 
-/* <module> Experiment file for learning cooking recipes.
+/* <module> Learn how to make an omelette with predicate invention.
 
-Usage instructions
-------------------
+This experiment file demonstrates the use of predicate invention to
+learn a program without recursion; how to unfold the learned program
+to remove invented predicates and make the originally large program
+easier to understand; and how to dynamically set configuration options.
 
-1. Set configuration option max_invented/1 to 3 with a
-set_configuration_option/2 directive:
 
+Running the example
+-------------------
+
+1. Known good configuration.
+
+The results listed below were obtained with the following configuration
+options, defined in configuration.pl and set in this file with
+set_configuration_option/2.
 ==
-auxiliaries:set_configuration_option(max_invented, [3]).
-==
-
-2. Run a learning query with a dynamic learning predicate to perform
-predicate invention:
-
-==
-?- learn_dynamic(recipe/2).
-recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
-'$1'(A,B):-whisk_eggs(A,C),'$2'(C,B).
-'$1'(A,B):-heat_oil(A,C),'$2'(C,B).
-'$1'(A,B):-break_eggs(A,C),'$2'(C,B).
-'$2'(A,B):-heat_oil(A,C),'$3'(C,B).
-'$2'(A,B):-whisk_eggs(A,C),'$3'(C,B).
-'$3'(A,B):-fry_eggs(A,C),season(C,B).
-recipe(A,B):-break_eggs(A,C),'$1'(C,B).
-recipe(A,B):-heat_oil(A,C),'$1'(C,B).
+?- list_config.
+depth_limits(2,1)
+example_clauses(call)
+experiment_file(data/examples/recipes.pl,recipes)
+fold_recursive(false)
+generalise_learned_metarules(false)
+learner(louise)
+learning_predicate(learn_dynamic/1)
+max_invented(2)
+metarule_formatting(user_friendly)
+metarule_learning_limits(none)
+minimal_program_size(2,inf)
+prove_recursive(invented)
+recursion_depth_limit(dynamic_learning,none)
+recursion_depth_limit(metasubstitution,none)
+recursive_reduction(false)
+reduce_learned_metarules(false)
+reduction(plotkins)
+resolutions(5000)
+test_constraints(clause)
+theorem_prover(resolution)
+unfold_invented(false)
 true.
+==
+
+If the output you see when you run the experiment differs from what is
+listed below make sure your configuration matches the listing above.
+
+In particular, make sure that the following options are set:
+==
+max_invented(2)
+prove_recursive(invented)
+==
+
+The max_invented(2) option tells Louise to try and define no more than 2
+invented predicates. prove_recursive(invented) tells Louise to learn by
+resolution with invented predicates. The latter option is the option
+that turns "on" predicate invention, proper, in Louise.
+
+
+2. MIL problem elements.
+
+Make sure that the output of list_mil_problem/1 matches the following:
+==
+?- list_mil_problem(recipe/2).
+Positive examples
+-----------------
+recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
+
+Negative examples
+-----------------
+[]
+
+Background knowledge
+--------------------
+break_eggs/2:
+break_eggs(A,B):-replace([eggs],A,[egg_whites,egg_yolks],B).
+
+whisk_eggs/2:
+whisk_eggs(A,B):-replace([egg_whisk,egg_whites,egg_yolks],A,[whisked_eggs],B).
+
+heat_oil/2:
+heat_oil(A,B):-replace([frying_pan,olive_oil],A,[frying_oil],B).
+
+fry_eggs/2:
+fry_eggs(A,B):-replace([frying_oil,whisked_eggs],A,[frying_eggs],B).
+
+season/2:
+season(A,B):-replace([frying_eggs,pepper,salt],A,[omelette],B).
+
+replace/4:
+replace(A,B,C,D):-ground(A),ground(B),ground(C),ord_subset(A,B),ord_subtract(B,A,E),ord_union(C,E,D).
+
+Metarules
+---------
+(Chain) ∃.P,Q,R ∀.x,y,z: P(x,y)← Q(x,z),R(z,y)
+true.
+==
+
+
+3. Make a learning attempt:
+==
+?- time(learn(recipe/2,_Ps)), print_clauses(_Ps), length(_Ps,N).
+% 4,387,466 inferences, 1.516 CPU in 1.596 seconds (95% CPU, 2894823 Lips)
+recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
+'$1'(A,B):-'$2'(A,C),'$2'(C,B).
+'$1'(A,B):-'$2'(A,C),heat_oil(C,B).
+'$1'(A,B):-'$2'(A,C),season(C,B).
+'$1'(A,B):-'$2'(A,C),whisk_eggs(C,B).
+'$1'(A,B):-break_eggs(A,C),'$2'(C,B).
+'$1'(A,B):-break_eggs(A,C),heat_oil(C,B).
+'$1'(A,B):-break_eggs(A,C),whisk_eggs(C,B).
+'$1'(A,B):-fry_eggs(A,C),season(C,B).
+'$1'(A,B):-heat_oil(A,C),'$2'(C,B).
+'$1'(A,B):-heat_oil(A,C),break_eggs(C,B).
+'$1'(A,B):-heat_oil(A,C),fry_eggs(C,B).
+'$1'(A,B):-heat_oil(A,C),whisk_eggs(C,B).
+'$1'(A,B):-whisk_eggs(A,C),'$2'(C,B).
+'$1'(A,B):-whisk_eggs(A,C),fry_eggs(C,B).
+'$1'(A,B):-whisk_eggs(A,C),heat_oil(C,B).
+'$2'(A,B):-'$1'(A,C),'$1'(C,B).
+'$2'(A,B):-'$1'(A,C),heat_oil(C,B).
+'$2'(A,B):-'$1'(A,C),season(C,B).
+'$2'(A,B):-'$1'(A,C),whisk_eggs(C,B).
+'$2'(A,B):-break_eggs(A,C),'$1'(C,B).
+'$2'(A,B):-break_eggs(A,C),heat_oil(C,B).
+'$2'(A,B):-break_eggs(A,C),whisk_eggs(C,B).
+'$2'(A,B):-fry_eggs(A,C),season(C,B).
+'$2'(A,B):-heat_oil(A,C),'$1'(C,B).
+'$2'(A,B):-heat_oil(A,C),break_eggs(C,B).
+'$2'(A,B):-heat_oil(A,C),fry_eggs(C,B).
+'$2'(A,B):-heat_oil(A,C),whisk_eggs(C,B).
+'$2'(A,B):-whisk_eggs(A,C),'$1'(C,B).
+'$2'(A,B):-whisk_eggs(A,C),fry_eggs(C,B).
+'$2'(A,B):-whisk_eggs(A,C),heat_oil(C,B).
+recipe(A,B):-'$1'(A,C),'$1'(C,B).
+recipe(A,B):-'$1'(A,C),'$2'(C,B).
+recipe(A,B):-'$1'(A,C),season(C,B).
+recipe(A,B):-'$2'(A,C),'$1'(C,B).
+recipe(A,B):-'$2'(A,C),'$2'(C,B).
+recipe(A,B):-'$2'(A,C),season(C,B).
+recipe(A,B):-break_eggs(A,C),'$1'(C,B).
+recipe(A,B):-break_eggs(A,C),'$2'(C,B).
+recipe(A,B):-heat_oil(A,C),'$1'(C,B).
+recipe(A,B):-heat_oil(A,C),'$2'(C,B).
+N = 41.
+==
+
+Note the use of time/1 and length/2 to keep track of the time it takes
+to learn the hypothesis above and its size. If your system is taking
+considerably longer to complete the learning attempt, you should
+probably examine your configuration and the elements of teh MIL problem
+for recipe/2 to make sure they match the ones listed above. The
+experiment listed above was run on a seven-year old laptop with an i7
+processor clocked at 2.6 GHz and 16 GB of RAM. You don't need a
+supercomputer to repeat it!
+
+
+3. Eyballing the result.
+
+First, find some eydrops. Ready? Now we can look more closely at the
+output of the learning attempt listed above. What we observe initially
+is that it is an unholly mess of invented predicates with meaningless
+symbols like '$1' that don't help us at all to understand what the heck
+the learned program is doing. Is it right? Is it wrong? Who can tell?
+
+We next observe that the single example of recipe/2 is output together
+with the learned program:
+==
+recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
+==
+
+This is normally a bad sign. Such "atomic residue" means that some
+positive examples are not "covered" by the learned program. In this case
+though it is a limitation of the implementation of Plotkin's program
+reduction in lib/program_reduction, which is very bad at dealing with
+lists. This is a known bug in other words.
+
+Still, how can we tell that the learned program is correct, or even what
+it is doing? That thing is nigh-on unreadable! Looking more carefully
+and ceasing our annoying complaints we can remark that the two invented
+predicates, '$1' and '$2' are almost identical in structure, with the
+difference that they each call the other. Their literals that don't have
+invented predicate symbols appear to be different combinations of
+preparation methods defined in the background ("heat oil", "break eggs"
+etc). Looking at the clauses with the target predicate's symbol,
+recipe/2, in their head, at the bottom of the listing, we can start to
+feel confident that the learned program seems to be taking alternative
+paths to the same destintation, making an omelette. In turn this means
+that there's no point in trying to reduce the program any further.
+Besides the limitatiosn of the implementation of Plotkin's program
+reduction, if each invented predicate is correctly defined, we won't
+find any logical redundancies.
+
+
+4. Unfolding to restore sanity and peace.
+
+Set the configuration option unfold_invented/1 to "true" to attempt to
+unfold the learned monstrosity regurgitated on our console in the
+previous section, and see if we can finally understand what it's trying
+to do:
+==
+?- time(learn(recipe/2,_Ps)), print_clauses(_Ps), length(_Ps,N).
+% 4,518,082 inferences, 1.625 CPU in 1.745 seconds (93% CPU, 2780358 Lips)
+recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
+recipe(A,B):-break_eggs(A,C),heat_oil(C,D),whisk_eggs(D,E),fry_eggs(E,F),season(F,B).
+recipe(A,B):-break_eggs(A,C),whisk_eggs(C,D),heat_oil(D,E),fry_eggs(E,F),season(F,B).
+recipe(A,B):-heat_oil(A,C),break_eggs(C,D),whisk_eggs(D,E),fry_eggs(E,F),season(F,B).
+N = 4.
+==
+
+Ah! What relief! The entire 41 clauses is now reduced to only 4, without
+any incomprehensible dollar-signs.
+
+Inspecting the unfolded program we can verify that it consists of three
+clauses that define different ways to make an omelette. We need to break
+eggs, of course, and we need to do that before we whisk the broken eggs.
+But we can heat the oil before or after either of those two steps. We
+must always fry the egggs and season the omelette as the final two steps
+of the procedure. Obviously, all this is true in the context of the
+definitions of the background predicates break_eggs/2, heat_oil/2 etc,
+that require specific inputs to succeed. For example, the break_eggs/2
+predicate expects the atom "eggs" to be in its input list, etc.
+
+
+Note that the reduction in size is the result of unfolding, only. While
+unfolded programs _are_ reduced at the end of er unfolding them, the
+original program did not include any reductions. You can verify this by
+setting the configuration option reduction/1 to "none" and repeating
+the learning query above:
+==
+?- time(learn(recipe/2,_Ps)), print_clauses(_Ps), length(_Ps,N), resolutions(Resolutions).
+% 2,777,803 inferences, 1.375 CPU in 1.466 seconds (94% CPU, 2020220 Lips)
+recipe([egg_whisk,eggs,frying_pan,olive_oil,pepper,salt],[omelette]).
+recipe(A,B):-break_eggs(A,C),heat_oil(C,D),whisk_eggs(D,E),fry_eggs(E,F),season(F,B).
+recipe(A,B):-break_eggs(A,C),whisk_eggs(C,D),heat_oil(D,E),fry_eggs(E,F),season(F,B).
+recipe(A,B):-heat_oil(A,C),break_eggs(C,D),whisk_eggs(D,E),fry_eggs(E,F),season(F,B).
+N = 4,
+Resolutions = 0.
 ==
 
 
@@ -51,12 +260,11 @@ This experiment file demonstrates the use of the auxiliary predicate
 set_configuration_option/2 to dynamically manipulate configuration
 options.
 
-The configuration option max_invented/1 is set dynamically using
-set_configuration_option/2 as a directive (at the start of the source
-code, below):
+In the source code below, the configuration option max_invented/1 is set
+dynamically using set_configuration_option/2 as a directive:
 
 ==
-auxiliaries:set_configuration_option(max_invented, [3]).
+auxiliaries:set_configuration_option(max_invented, [2]).
 ==
 
 Note that setting a configuration option dynamically using
@@ -70,7 +278,7 @@ option to its original value is to edit the value of that option in the
 configuration file and then reload the configuration file with make/0.
 */
 
-:- auxiliaries:set_configuration_option(max_invented, [3]).
+:- auxiliaries:set_configuration_option(max_invented, [2]).
 
 % Tells list_learning_results/0 to use the right learning predicate.
 configuration:learning_predicate(learn_dynamic/1).
@@ -133,8 +341,7 @@ replace(Xs,Is,Ys,Os):-
 	,ord_union(Ys,Zs_,Os).
 
 
-% Target theory for omelette
-% Thelma learns a better one with a bit of invention.
+% Target theory for an omelette recipe.
 recipe_(As,Fs):-
 	break_eggs(As,Bs)
 	,whisk_eggs(Bs,Cs)
