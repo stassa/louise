63752e3283480282dad20b317b0fa91870e9b882
YeGoblynQueenne <ep2216@ic.ac.uk>
2023-08-22 11:59:49 +0100

    Renamed minimal_program.pl --> greedy.pl. * The name "minimal program", and the names of predicates defined in   that module, learn_minimal/[1,2,5], was a misnomer: there is no   guarantee of minimality in those learning predicates. Rather, the   learning strategy is a greedy learning strategy that consumes all   examples entailed by the hypothesis so-far before adding a new clause   to it. Hence, the module is renamed to "greedy" and the learn_minimal   predicates are renamed to learn_greedy. * Note that this module and these learning predicates do not yet use   Vanilla, rather they are still on the older, single-clause learning,   version of TPC. More work is needed to implement greedy learning with   Vanillla.

--- src/subsystems/toil.pl
+++ src/subsystems/toil.pl
@@ -8,7 +8,7 @@
 
 :-use_module(project_root(configuration)).
 :-use_module(src(auxiliaries)).
-:-use_module(subsystems(minimal_program)).
+:-use_module(subsystems(greedy)).
 :-use_module(lib(lifting/lifting)).
 :-use_module(src(mil_problem)).
 :-use_module(src(vanilla)).
